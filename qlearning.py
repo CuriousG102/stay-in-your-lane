a5dbc4722c8609a9d78826afb654da70940d3510
Kenneth Hoffmann <kennethhoffmann@Kenneths-MacBook-Pro.local>
2017-11-23 01:29:20 -0600

    updated thresholding and lines

--- qlearning.py
+++ qlearning.py
@@ -36,7 +36,73 @@ def determine_reward(state):
     reward = rewards[state]
     return reward
 
-    # Send to the simulator
+def average_slope_intercept(lines):
+    left_lines    = [] # (slope, intercept)
+    left_weights  = [] # (length,)
+    right_lines   = [] # (slope, intercept)
+    right_weights = [] # (length,)
+    
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            if x2==x1:
+                continue # ignore a vertical line
+            slope = (y2-y1)/(x2-x1)
+            intercept = y1 - slope*x1
+            length = np.sqrt((y2-y1)**2+(x2-x1)**2)
+            if slope < 0: # y is reversed in image
+                left_lines.append((slope, intercept))
+                left_weights.append((length))
+            else:
+                right_lines.append((slope, intercept))
+                right_weights.append((length))
+    
+    # add more weight to longer lines    
+    left_lane  = np.dot(left_weights,  left_lines) /np.sum(left_weights)  if len(left_weights) >0 else None
+    right_lane = np.dot(right_weights, right_lines)/np.sum(right_weights) if len(right_weights)>0 else None
+    
+    return left_lane, right_lane # (slope, intercept), (slope, intercept)
+
+def make_line_points(y1, y2, line):
+    """
+    Convert a line represented in slope and intercept into pixel points
+    """
+    if line is None:
+        return None
+    
+    slope, intercept = line
+    
+    # make sure everything is integer as cv2.line requires it
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    y1 = int(y1)
+    y2 = int(y2)
+    
+    return ((x1, y1), (x2, y2))
+
+def lane_lines(image, lines):
+    left_lane, right_lane = average_slope_intercept(lines)
+    
+    y1 = image.shape[0] # bottom of the image
+    y2 = y1*0.6         # slightly lower than the middle
+
+    left_line  = make_line_points(y1, y2, left_lane)
+    right_line = make_line_points(y1, y2, right_lane)
+    
+    return left_line, right_line
+
+    
+def draw_lane_lines(image, lines, color=[255, 0, 0], thickness=20):
+    # make a separate image to draw lines and combine with the orignal later
+    line_image = np.zeros_like(image)
+    for line in lines:
+        if line is not None:
+            print(*line)
+            cv2.line(line_image, (line[0][0], line[0][1] ),( line[0][2] ,line[0][3]),  color, thickness)
+    # image1 * α + image2 * β + λ
+    # image1 and image2 must be the same shape.
+    return cv2.addWeighted(image, 1.0, line_image, 0.95, 0.0)
+             
+    
 
 
 def main():
@@ -54,15 +120,23 @@ def main():
     action_step = .2
     control_step = .1
     dt = .02
-    min_threshold = 35
+    min_threshold = 25
     max_threshold = 2 * min_threshold
     kernel_size = 5
+    lower = np.uint8([ 90, 120,120])
+    upper = np.uint8([ 126,209, 215]) # 180,228, 228
+
 
     s = SimClient()
     s.start()
     # Intialize Q table based on number of rows and actions
     Q = np.zeros([NUM_states, NUM_actions])
     cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('Yellow', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('blur', cv2.WINDOW_NORMAL)
+
+
+
 
     for episode in range(episode_number):
         # Intialize the sim also put a reset in here somehwere
@@ -70,7 +144,7 @@ def main():
         action = 0
         state = 0
         next_state = 0
-        current_steering = 0
+        current_steering = -.03
         constant_throttle = 1
         control_time = 0
         t = 0
@@ -89,15 +163,21 @@ def main():
             open_cv_image = np.array(observation.front_camera_image)
             open_cv_image = open_cv_image[:, :, ::-1].copy()
             #open_cv_image = np.array(observation.overhead_camera_image)
+            
+            
+            blurred = cv2.GaussianBlur(open_cv_image, (kernel_size, kernel_size), 0)
+            yellow_only = cv2.inRange(blurred, lower, upper)
+            cv2.imshow('blur', blurred)
+            cv2.imshow('Yellow', yellow_only)
+            #canny_edges = cv2.Canny(blurred, min_threshold, max_threshold)
+            ret, thresh = cv2.threshold(yellow_only, 120, 255, cv2.THRESH_BINARY)
+            canny_thresh_edges = cv2.Canny(thresh, min_threshold, max_threshold)
+            #update below code
+            lines = cv2.HoughLinesP(canny_thresh_edges,rho = 1,theta = 1*np.pi/180,threshold = 30,minLineLength = 25,maxLineGap = 40)
 
-            blurred = cv2.GaussianBlur(
-                open_cv_image, (kernel_size, kernel_size), 0)
-            canny_edges = cv2.Canny(blurred, min_threshold, max_threshold)
-            ret, thresh = cv2.threshold(
-                open_cv_image, 120, 255, cv2.THRESH_BINARY)
-            #canny_thresh_edges = cv2.Canny(thresh, min_threshold, max_threshold)
-            #cv2.imshow('Display', canny_edges)
-            cv2.imshow('Display', thresh)
+            output=draw_lane_lines(canny_thresh_edges, lines)
+            #print(left_lane,right_lane)
+            cv2.imshow('Display', output)
             #cv2.imshow('Display', canny_thresh_edges)
             cv2.waitKey(1)
 
