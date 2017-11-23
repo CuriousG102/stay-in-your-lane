f4f3b3411b5d5ab5dd31322a09645b58d5e22e61
Aviv Elbag <aviv_elbag@umail.ucsb.edu>
2017-11-22 18:26:18 -0800

    Made square grid in the process of making mapping

--- Transforms.py
+++ Transforms.py
@@ -19,6 +19,8 @@ def Flatten_Car_Image(Image,Pixels):
     transformed_Image = cv2.warpPerspective(img,M,(300,300))
     return transformed_Image
 
+
+
 def Find_Pixels(Image):
     '''
     :param Image: Takes a cv2.Image object
@@ -33,15 +35,32 @@ def show_Image(Image):
     '''
     plt.plot(Image)
 
+def make_squares(n,m):
+    B = np.zeros((n,n))
+    for X in range(n):
+        for Y in range(n):
+            B[X,Y] = np.floor((np.floor(X/(n/m))+np.floor(Y/(n/m)))%2)
+    # img = cv2.imread(B)
+    # rows, cols, ch = img.shape
+    # pts1 = np.float32([[n-1, n-1], [0, n-1], [n-1, 0], [0, 0]])
+    # pts2 = pts2 = np.float32([[300, 300], [0, 300], [300, 0], [0, 0]])  # Not exactly the best transform yet
+    # M = cv2.getPerspectiveTransform(pts1, pts2)
+    # transformed_Image = cv2.warpPerspective(img, M, (300, 300))
+    plt.imshow(B)
+    plt.show()
+
 
 if __name__ == '__main__':
-    Image = 'rail.jpg'
-    Pixels = [[1000,730],[100,730],[700,500],[300,500]]
-    Example = Flatten_Car_Image(Image,Pixels)
-    img = cv2.imread(Image)
-    plt.subplot(121), plt.imshow(img), plt.title('Input')
-    plt.subplot(122), plt.imshow(Example), plt.title('Output')
-    plt.show()
+    # Image = 'rail.jpg'
+    # Pixels = [[1000,730],[100,730],[700,500],[300,500]]
+    # Example = Flatten_Car_Image(Image,Pixels)
+    # img = cv2.imread(Image)
+    # plt.subplot(121), plt.imshow(img), plt.title('Input')
+    # plt.subplot(122), plt.imshow(Example), plt.title('Output')
+    # plt.show()
+    make_squares(512,16)
+
+
 
 
 
