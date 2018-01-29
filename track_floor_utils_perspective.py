Correct a couple off by one errors and add some configurability

track_floor_utils_perspective.py
@@ -253,11 +253,11 @@ def get_car_view_img(position, rotation):
         (int(IMG_SIZE_X), int(IMG_SIZE_Z)))
     return cropped_img & IMG_VISIBLE_MASK
 
-def car_img_to_top_down_perspective(car_img):
+def car_img_to_top_down_perspective(car_img, img_portion):
     z_imax, x_imax, _ = car_img.shape
     assert(z_imax == x_imax)
     replot_img = np.zeros((int(IMG_SIZE_Z), int(IMG_SIZE_X), 3), dtype=np.uint8)
-    for z_img in range(int(z_imax*.7)):
+    for z_img in range(int(z_imax*img_portion)):
         z_ifrac = z_img / z_imax
         z_plot = (
             CAR_CAM_HEIGHT 
@@ -266,17 +266,17 @@ def car_img_to_top_down_perspective(car_img):
                        - z_ifrac * math.radians(CAR_CAM_FOV)))
         z_pfrac = (z_plot - L_MIN) / (L_MAX - L_MIN)
         # print('z: ', z_pfrac)
-        assert z_pfrac >= 0, z_pfrac <= 1
+        # assert z_pfrac >= 0, z_pfrac <= 1
         z_w = int(IMG_SIZE_Z) - int(z_pfrac * int(IMG_SIZE_Z)) - 1
         for x_img in range(x_imax):
             x_ifrac = x_img / x_imax
             x_plot = 2 * (x_ifrac - .5) * math.tan(math.radians(CAR_CAM_FOV) / 2) * z_plot
             x_pfrac = .5 * (x_plot / X_U + 1)
             # print('x: ', x_pfrac)
-            assert x_pfrac >= 0, x_pfrac <= 1
-            x_w = int(x_pfrac * int(IMG_SIZE_X))
+            # assert x_pfrac >= 0, x_pfrac <= 1
+            x_w = int(x_pfrac * int(IMG_SIZE_X)) - 1
 
-            print(x_w, z_w)
+            # print(x_w, z_w)
             replot_img[z_w, x_w] = car_img[z_imax - z_img - 1, x_img]
 
     return replot_img