e87ab41447d8a480a896ca6a5032494fcce1f8ef
Miles Hutson <mileshutson@gmail.com>
2018-02-01 23:57:57 -0800

    More sophisticated image utils

--- image_utils.py
+++ image_utils.py
@@ -14,6 +14,25 @@ def simple_threshold(img):
                 for color_channel in (b, g, r))
     return bt | gt | rt
 
+class ThresholdImages:
+    def __init__(self, outside, inside):
+        self.outside = outside
+        self.inside = inside
+
+def simple_threshold_sides(img):
+    b,g,r = (img[:, :, i] for i in range(3))
+    bt,gt,rt = (cv2.threshold(color_channel,
+                              120, 255, cv2.THRESH_BINARY)[1]
+                for color_channel in (b, g, r))
+    return ThresholdImages(bt | gt & (~rt), bt | gt | rt)
+
+def simple_threshold_ternary(img):
+    ternary = np.zeros(img.shape, dtype=np.uint8)
+    thresh_sides = simple_threshold_sides(img)
+    ternary[:, :, 2] = thresh_sides.outside
+    ternary[:, :, 1] = thresh_sides.inside
+    return ternary
+
 def crop_img_from_below(img, num_rows):
     return img[num_rows:, :, :]
 
